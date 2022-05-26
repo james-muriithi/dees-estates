458c56be2d2366e7689d322891d5e40b5166b8b7
james-muriithi <muriithijames556@gmail.com>
2022-05-27 01:42:46 +0300

    added footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,73 @@
+<template>
+  <footer class="py-20 mt-6 bg-primary">
+    <div class="container mx-auto grig grid-cols-3 md:grid-cols-4 gap-10 px-3">
+      <div>
+        <img src="@images/logolight.svg" class="h-5 mb-4" alt="Dees estates" />
+        <p class="text-gray-300">
+          Solution for easy and flexible house hunting. You can trust us
+          anywhere through this platform
+        </p>
+      </div>
+
+      <div class="md:flex mt-8 md:mt-0">
+        <div class="mx-auto">
+          <h3 class="text-xl text-light font-bold mb-3">Property</h3>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Learning Modules
+          </router-link>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Partnership
+          </router-link>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Watch demo
+          </router-link>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Event
+          </router-link>
+        </div>
+      </div>
+
+      <div class="md:flex mt-8 md:mt-0">
+        <div class="mx-auto">
+          <h3 class="text-xl text-light font-bold mb-3">Property</h3>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Learning Modules
+          </router-link>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Partnership
+          </router-link>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Watch demo
+          </router-link>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Event
+          </router-link>
+        </div>
+      </div>
+
+       <div class="md:flex mt-8 md:mt-0">
+        <div class="mx-auto">
+          <h3 class="text-xl text-light font-bold mb-3">Property</h3>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Learning Modules
+          </router-link>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Partnership
+          </router-link>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Watch demo
+          </router-link>
+          <router-link to="#" class="text-gray-300 block mb-2">
+            Event
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "PageFooter",
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,16 +5,25 @@
   <WhyUs />
   <FeaturedProperties />
   <Testimonials />
+  <PageFooter />
 </template>
 
 <script>
 import Banner from "../components/Banner.vue";
-import HowItWorks from '../components/HowItWorks.vue';
-import WhyUs from '../components/WhyUs.vue';
+import HowItWorks from "../components/HowItWorks.vue";
+import WhyUs from "../components/WhyUs.vue";
 import FeaturedProperties from "../components/FeaturedProperties.vue";
-import Testimonials from '../components/Testimonials.vue';
+import Testimonials from "../components/Testimonials.vue";
+import PageFooter from "../components/PageFooter.vue";
 export default {
-  components: { Banner, HowItWorks, WhyUs, FeaturedProperties, Testimonials },
+  components: {
+    Banner,
+    HowItWorks,
+    WhyUs,
+    FeaturedProperties,
+    Testimonials,
+    PageFooter,
+  },
   name: "Home",
 };
 </script>
